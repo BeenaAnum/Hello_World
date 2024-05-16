f894c6f85074ebbe37b400361d7010aed58600d2
AI Anam <160389592+BeenaAnum@users.noreply.github.com>
2024-05-16 14:47:10 +0500

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,12 @@
+console.log("Hello World");
+console.log("Thankyou Governer Sindh kamran Khan Tessori To This Initiatives");
+console.log("I'm the Student of your IT class at monday morning 9:00 am");
+console.log("now today's my 2nd class at Governor house")
+console.log("Hello Everyone")
+console.log("Thankyou Everyone");
+console.log("how old are you");
+console.log("this is my 1st and 2nd class work") 
+
+//In this class we learn how create src folder , dist folder and create tsconfig.json file
+//two type of creating dist (distribution) folder or directory fist is by the tsconfig.json file using this command (tsc --init) and
+//second is that by this command (tsc main.ts or index.ts --outDir dist) in this command first we create ts file then create dist folder by tsconfig.json.
